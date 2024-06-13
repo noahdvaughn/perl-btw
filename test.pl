f81ac04c91297f59d015cfaeb5935b7ea84d90db
Noah Vaughn <noahdvaughn@gmail.com>
2024-06-12 20:04:40 -0400

    data types

--- test.pl
+++ test.pl
@@ -2,5 +2,18 @@
 
 $message = 'Hello, world';
 
-print "$message\n";
+
+$scalar = 3;
+@array = (1,2,3);
+%hash = ('name', 'noah');
+
+$var = <<"EOF";
+$message
+Example of scalar: $scalar
+Example of array: @array
+Example of hash: $hash{'name'}
+EOF
+print "$var\n";
+
+
 
