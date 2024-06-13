9c6029aa6d2e78e623b1eae02fc3bcca4e929bd0
Noah Vaughn <noahdvaughn@gmail.com>
2024-06-12 20:23:50 -0400

    refs are neat

--- test.pl
+++ test.pl
@@ -2,9 +2,12 @@
 
 $message = 'Hello, world';
 
-
+#scalar is like a simple var: int, string, float
 $scalar = 3;
+
 @array = (1,2,3);
+
+#key value pair, unordered
 %hash = ('name', 'noah');
 
 $var = <<"EOF";
@@ -14,12 +17,15 @@ Example of array: @array
 Example of hash: $hash{'name'}
 EOF
 
-
+# no functions here only subroutines
 sub testSubroutine{
   print "$var\n";
 }
 
-testSubroutine();
+# Before perl5 was called with & but only in references now, use PascalCase
+TestSubroutine();
+
 
 ($scalar > 2) ? print "Thats a big scalar\n" : print "Thats a little scalar\n";
 
+$scalarreference = \$scalar;
